3d54217e96fef88532829ccde67b07bef332f946
Chipster <go@w.ay>
2020-12-29 20:25:34 -0600

    JT improvement, some from F1RMB (looks like we think alike!)

--- admin/expert/jitter_test.php
+++ admin/expert/jitter_test.php
@@ -1,9 +1,16 @@
 <?php
+if (isset($_COOKIE['PHPSESSID']))
+{
+    session_id($_COOKIE['PHPSESSID']); 
+}
+if (session_status() != PHP_SESSION_ACTIVE) {
+    session_start();
+}
 
-if (!isset($_SESSION) || !is_array($_SESSION)) {
+if (!isset($_SESSION) || !is_array($_SESSION) || (count($_SESSION, COUNT_RECURSIVE) < 10)) {
     session_id('pistardashsess');
     session_start();
-
+    
     include_once $_SERVER['DOCUMENT_ROOT'].'/config/config.php';          // MMDVMDash Config
     include_once $_SERVER['DOCUMENT_ROOT'].'/mmdvmhost/tools.php';        // MMDVMDash Tools
     include_once $_SERVER['DOCUMENT_ROOT'].'/mmdvmhost/functions.php';    // MMDVMDash Functions
@@ -11,12 +18,11 @@ if (!isset($_SESSION) || !is_array($_SESSION)) {
     checkSessionValidity();
 }
 
-// Load the Pi-Star Release file
-$pistarReleaseConfig = '/etc/pistar-release';
-$configPistarRelease = array();
-$configPistarRelease = parse_ini_file($pistarReleaseConfig, true);
-// Load the Version Info
-require_once('../config/version.php');
+include_once $_SERVER['DOCUMENT_ROOT'].'/config/config.php';          // MMDVMDash Config
+include_once $_SERVER['DOCUMENT_ROOT'].'/mmdvmhost/tools.php';        // MMDVMDash Tools
+include_once $_SERVER['DOCUMENT_ROOT'].'/mmdvmhost/functions.php';    // MMDVMDash Functions
+include_once $_SERVER['DOCUMENT_ROOT'].'/config/language.php';	      // Translation Code
+include_once $_SERVER['DOCUMENT_ROOT'].'/config/version.php';
 
 // Sanity Check that this file has been opened correctly
 if ($_SERVER["PHP_SELF"] == "/admin/expert/jitter_test.php") {
@@ -27,61 +33,63 @@ if ($_SERVER["PHP_SELF"] == "/admin/expert/jitter_test.php") {
     if ($_GET['group'] == "hblink")       { $target = "HB"; }
   } else { $target = ""; }
 
-  if (!isset($_GET['ajax'])) {
-    system('sudo touch /var/log/pi-star/pi-star_icmptest.log > /dev/null 2>&1 &');
-    system('sudo echo "" > /var/log/pi-star/pi-star_icmptest.log > /dev/null 2>&1 &');
-    system('sudo /usr/local/sbin/pistar-jittertest '.$target.' > /dev/null 2>&1 &');
-  }
-
-  // Sanity Check Passed.
-  header('Cache-Control: no-cache');
-
-  if (!isset($_GET['ajax'])) {
-    //unset($_SESSION['update_offset']);
-    if (file_exists('/var/log/pi-star/pi-star_icmptest.log')) {
-      $_SESSION['update_offset'] = filesize('/var/log/pi-star/pi-star_icmptest.log');
-    } else {
-      $_SESSION['update_offset'] = 0;
+    if (!isset($_GET['ajax'])) {
+	system('sudo touch /var/log/pi-star/pi-star_icmptest.log > /dev/null 2>&1 &');
+	system('sudo echo "" > /var/log/pi-star/pi-star_icmptest.log > /dev/null 2>&1 &');
+	system('sudo /usr/local/sbin/pistar-jittertest '.$target.' > /dev/null 2>&1 &');
+	$_SESSION['jittertest-isrunning'] = 1;
     }
-  }
-
-  if (isset($_GET['ajax'])) {
-    //session_start();
-    if (!file_exists('/var/log/pi-star/pi-star_icmptest.log')) {
-      exit();
-  }
-
-  if (($handle = fopen('/var/log/pi-star/pi-star_icmptest.log', 'rb')) != false) {
-    if (isset($_SESSION['update_offset'])) {
-	fseek($handle, 0, SEEK_END);
-	if ($_SESSION['update_offset'] > ftell($handle)) { //log rotated/truncated
-	    $_SESSION['update_offset'] = 0; //continue at beginning of the new log
+    
+    // Sanity Check Passed.
+    header('Cache-Control: no-cache');
+    
+    if (!isset($_GET['ajax'])) {
+	//unset($_SESSION['update_offset']);
+	if (file_exists('/var/log/pi-star/pi-star_icmptest.log')) {
+	    $_SESSION['update_offset'] = filesize('/var/log/pi-star/pi-star_icmptest.log');
 	}
-
-	$data = stream_get_contents($handle, -1, $_SESSION['update_offset']);
-
-	$jitterIsRunning = shell_exec('ps ax | grep "/usr/local/sbin/pistar-jittertest" | grep -v grep') != null ? "YES" : "NO";
-	$oldOffset = $_SESSION['update_offset'];
-
-	$_SESSION['update_offset'] += strlen($data);
-	echo "<pre>$data</pre>";
+	else {
+	    $_SESSION['update_offset'] = 0;
+	}
+    }
+    
+    if (isset($_GET['ajax'])) {
+	//session_start();
+	if (!file_exists('/var/log/pi-star/pi-star_icmptest.log')) {
+	    exit();
+	}
 	
-	// we reach the end of the test
-	if (($oldOffset == $_SESSION['update_offset']) && (isset($_SESSION['jittertest-isrunning']) && ($_SESSION['jittertest-isrunning'] == 1)) && ($jitterIsRunning == "NO"))
-	{
-	    unset($_SESSION['jittertest-isrunning']);
-	    echo "<pre>Test Complete</pre>";
+	if (($handle = fopen('/var/log/pi-star/pi-star_icmptest.log', 'rb')) != false) {
+	    if (isset($_SESSION['update_offset'])) {
+		fseek($handle, 0, SEEK_END);
+		if ($_SESSION['update_offset'] > ftell($handle)) { //log rotated/truncated
+		    $_SESSION['update_offset'] = 0; //continue at beginning of the new log
+		}
+		
+		$data = stream_get_contents($handle, -1, $_SESSION['update_offset']);
+		
+		$jitterIsRunning = shell_exec('ps ax | grep "/usr/local/sbin/pistar-jittertest" | grep -v grep') != null ? "YES" : "NO";
+		$oldOffset = $_SESSION['update_offset'];
+		
+		$_SESSION['update_offset'] += strlen($data);
+		echo "<pre>$data</pre>";
+		
+		// we reach the end of the test
+		if (($oldOffset == $_SESSION['update_offset']) && (isset($_SESSION['jittertest-isrunning']) && ($_SESSION['jittertest-isrunning'] == 1)) && ($jitterIsRunning == "NO"))
+		{
+		    unset($_SESSION['jittertest-isrunning']);
+		    echo "<pre>Test Has Ended</pre>";
+		}
+	    }
+	    else {
+		fseek($handle, 0, SEEK_END);
+		$_SESSION['update_offset'] = ftell($handle);
+	    }
 	}
+	exit();
     }
-    else {
-	fseek($handle, 0, SEEK_END);
-	$_SESSION['update_offset'] = ftell($handle);
-    }
-  }
-  exit();
-}
-
 ?>
+
   <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
   <html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" lang="en">
@@ -123,9 +131,9 @@ if ($_SERVER["PHP_SELF"] == "/admin/expert/jitter_test.php") {
   <div class="contentwide">
   <form method="get" action="<?php ($_SERVER["PHP_SELF"]); ?>">
   <div>Select network to test: 
-	[ BrandMeister: <input type="radio" value="brandmeister" name="group" /> ]
-	[ HB-Link: <input type="radio" value="hblink" name="group" /> ]
-	[ DMR+: <input type="radio" value="dmrplus" name="group" /> ] <input type="submit" name="sumbit" value="Start Test" />
+	[ BrandMeister: <input type="radio" value="brandmeister" name="group" /> |
+	HB-Link: <input type="radio" value="hblink" name="group" /> |
+	DMR+: <input type="radio" value="dmrplus" name="group" /> ] <input type="submit" name="sumbit" value="Start Test" />
   </form>
   <table width="100%">
   <?php if (empty($target)) { ?>
