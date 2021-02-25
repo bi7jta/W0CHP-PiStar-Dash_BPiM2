dde74d5981e6ae4c2e3e09f72f028a100725cf91
Chipster <go@w.ay>
2021-02-25 16:25:14 -0600

    verbiage tweaks for accuracy

--- mmdvmhost/repeaterinfo.php
+++ mmdvmhost/repeaterinfo.php
@@ -501,7 +501,7 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/config/ircddblocal.php');
         echo "<tr><th>Pool</th>\n";
         echo "<td style=\"background: #ffffff;\" title=\"".$_SESSION['APRSGatewayConfigs']['APRS-IS']['Server']."\">".substr($_SESSION['APRSGatewayConfigs']['APRS-IS']['Server'], 0, 23)."</td></tr>\n";
         echo "<tr><th colspan='2' >Connected Server</th></tr>\n";
-        echo "<td style=\"background: #ffffff;\" title=\"".getAPRSISserver()."\">".getAPRSISserver()."</td></tr>\n";
+        echo "<td colspan='2' style=\"background: #ffffff;\" title=\"".getAPRSISserver()."\">".getAPRSISserver()."</td></tr>\n";
         echo "</table>\n";
     }
 
