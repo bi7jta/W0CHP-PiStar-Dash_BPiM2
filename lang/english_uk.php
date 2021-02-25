36df9277bb1afa321b7f415af4e1b9f94e7054fd
Chipster <go@w.ay>
2021-02-25 16:35:49 -0600

    en lang update for APRF GW

--- lang/english_uk.php
+++ lang/english_uk.php
@@ -82,7 +82,7 @@ $lang = array (
   "dstar_rpt2"                  =>  "RPT2 Callsign",
   "dstar_irc_password"          =>  "Remote Password",
   "dstar_default_ref"           =>  "Default Reflector",
-  "aprs_host"                   =>  "APRS Host Pool",
+  "aprs_host"                   =>  "APRS Gateway Host Pool",
   "dstar_irc_lang"              =>  "ircDDBGateway Language",
   "dstar_irc_time"              =>  "Time Announcements",
   // Config Page - YSF Configuration
