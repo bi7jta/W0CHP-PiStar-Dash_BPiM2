b688b3118f25bc3d3271c34c8deb69946944c8da
Chipster <go@w.ay>
2021-06-20 07:42:29 -0500

    Merge upstream change

--- admin/configure.php
+++ admin/configure.php
@@ -1978,7 +1978,7 @@ if (!empty($_POST)):
 	  $uPNPsvcOn = 'sudo systemctl enable pistar-upnp.timer';
 	  $uPNPsvcOff = 'sudo systemctl disable pistar-upnp.timer';
 	  $uPNPsvcStart = '(systemctl stop pistar-upnp.service && systemctl start pistar-upnp.service) &';
-	  $uPNPsvcStop = 'systemctl stop pistar-upnp.service';
+	  $uPNPsvcStop = '(systemctl stop pistar-upnp.service) &';
 
 	  if (escapeshellcmd($_POST['uPNP']) == 'ON' )  { system($uPNPon); system($uPNPsvcOn); system($uPNPsvcStart); }
 	  if (escapeshellcmd($_POST['uPNP']) == 'OFF' ) { system($uPNPoff); system($uPNPsvcStop); system($uPNPsvcOff); }
