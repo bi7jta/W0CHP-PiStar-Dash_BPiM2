d0ae19c4f052dfdc8f7f122e9433238fbad5ad04
Chipster <go@w.ay>
2021-10-24 15:36:58 -0500

    scaling back log lines for efficiency and better update timing

--- mmdvmhost/functions.php
+++ mmdvmhost/functions.php
@@ -449,18 +449,18 @@ function getMMDVMLog() {
     $logLines2 = array();
     if (file_exists(MMDVMLOGPATH."/".MMDVMLOGPREFIX."-".gmdate("Y-m-d").".log")) {
 	$logPath = MMDVMLOGPATH."/".MMDVMLOGPREFIX."-".gmdate("Y-m-d").".log";
-	$logLines1 = explode("\n", `egrep -h "from|end|watchdog|lost" $logPath | sed '/\(CSBK\|overflow\|Downlink\)/d' | tail -500`);
+	$logLines1 = explode("\n", `egrep -h "from|end|watchdog|lost" $logPath | sed '/\(CSBK\|overflow\|Downlink\)/d' | tail -350`);
     }
-    $logLines1 = array_slice($logLines1, -500);
+    $logLines1 = array_slice($logLines1, -350);
     if (sizeof($logLines1) < 350) {
 	if (file_exists(MMDVMLOGPATH."/".MMDVMLOGPREFIX."-".gmdate("Y-m-d", time() - 86340).".log")) {
 	    $logPath = MMDVMLOGPATH."/".MMDVMLOGPREFIX."-".gmdate("Y-m-d", time() - 86340).".log";
-	    $logLines2 = explode("\n", `egrep -h "from|end|watchdog|lost" $logPath | sed '/\(CSBK\|overflow\|Downlink\)/d' | tail -500`);
+	    $logLines2 = explode("\n", `egrep -h "from|end|watchdog|lost" $logPath | sed '/\(CSBK\|overflow\|Downlink\)/d' | tail -350`);
 	}
     }
-    $logLines2 = array_slice($logLines2, -500);
+    $logLines2 = array_slice($logLines2, -350);
     $logLines = $logLines1 + $logLines2;
-    $logLines = array_slice($logLines, -500);
+    $logLines = array_slice($logLines, -350);
     return $logLines;
 }
 
