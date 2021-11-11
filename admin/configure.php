3c33673cec145ab38d7ec34acc7d37c1d739a13a
Chipster <go@w.ay>
2021-11-11 15:59:56 -0600

    Merge upstream changes

--- admin/configure.php
+++ admin/configure.php
@@ -1051,7 +1051,12 @@ if (!empty($_POST)):
 		}
 	  	else {
 			$configysfgateway['Network']['Startup'] = $newYSFStartupHostArr[1];
-			$configdmr2ysf['DMR Network']['DefaultDstTG'] = str_replace("FCS", "1", $newYSFStartupHostArr[0]);
+			if (substr( $newYSFStartupHostArr[0], 0, 3 ) !== "FCS") {
+				$configdmr2ysf['DMR Network']['DefaultDstTG'] = $newYSFStartupHostArr[0];
+			} else {
+				$configdmr2ysf['DMR Network']['DefaultDstTG'] = "9";
+			}
+			//$configdmr2ysf['DMR Network']['DefaultDstTG'] = str_replace("FCS", "1", $newYSFStartupHostArr[0]);
 		}
 	  } else {
 	  	if ($newYSFStartupHostArr[0] == "none") {
@@ -1060,7 +1065,12 @@ if (!empty($_POST)):
 		}
 	  	else {
 			$configysfgateway['Network']['Startup'] = $newYSFStartupHostArr[0];
-			$configdmr2ysf['DMR Network']['DefaultDstTG'] = str_replace("FCS", "1", $newYSFStartupHostArr[0]);
+			if (substr( $newYSFStartupHostArr[0], 0, 3 ) !== "FCS") {
+				$configdmr2ysf['DMR Network']['DefaultDstTG'] = $newYSFStartupHostArr[0];
+			} else {
+				$configdmr2ysf['DMR Network']['DefaultDstTG'] = "9";
+			}
+			//$configdmr2ysf['DMR Network']['DefaultDstTG'] = str_replace("FCS", "1", $newYSFStartupHostArr[0]);
 		}
 	  }
 	}
