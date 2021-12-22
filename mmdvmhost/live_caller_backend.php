05197eebc90f660fefd64980b7ea5c5623f02fb2
Chipster <go@w.ay>
2021-12-22 11:27:50 -0600

    code cleaning

--- mmdvmhost/live_caller_backend.php
+++ mmdvmhost/live_caller_backend.php
@@ -97,10 +97,10 @@ $country = exec("grep -w \"$listElem[2]\" /usr/local/etc/stripped.csv | awk -F, 
 if (strlen($target) >= 2) {
 	$target_lookup = exec("grep -w \"$target\" /usr/local/etc/groups.txt | awk -F, '{print $1}' | head -1 | tr -d '\"'");
 	if (!empty($target_lookup)) {
-		$target = $target_lookup;
+        $target = $target_lookup;
         $stupid_bm = ['/ - 10 Minute Limit/', '/ NOT A CALL CHANNEL/', '/ NO NETS\!\!\!/', '/ - .*/'];
         $target = preg_replace($stupid_bm, "", $target);
-		$target = str_replace(":", " - ", $target);
+        $target = str_replace(":", " - ", $target);
 	}
 }
 if (strpos($mode, 'DMR') !== false) {
