217b2e290b0d036ee6bd6c6f71da41e83337c1f3
Chipster <go@w.ay>
2021-12-22 18:10:02 -0600

    finally got the proper concat logic (I think) :-/

--- config/version.php
+++ config/version.php
@@ -1,6 +1,6 @@
 <?php
 // Enhanced version status; W0CHP
-$ver_no = '20211222_01-W0CHP';
+$ver_no = '20211222_02-W0CHP';
 //
 //
 //
--- mmdvmhost/functions.php
+++ mmdvmhost/functions.php
@@ -492,7 +492,7 @@ function getMMDVMLog() {
             $logLines1 = array_slice($logLines1, -150);
         }
     }
-    if ($lineNos < 25) {
+    if ($lineNos < 150) {
         if (file_exists(MMDVMLOGPATH."/".MMDVMLOGPREFIX."-".gmdate("Y-m-d", time() - 86340).".log")) {                                                                             
             $logPath = MMDVMLOGPATH."/".MMDVMLOGPREFIX."-".gmdate("Y-m-d", time() - 86340).".log";
             if (!file_exists("/etc/.GETNAMES")) {
@@ -504,7 +504,7 @@ function getMMDVMLog() {
             }
         }
     }
-    if ($lineNos < 25) {
+    if ($lineNos < 150) {
         $logLines = $logLines1 + $logLines2;
     } else {
         $logLines = $logLines1;
