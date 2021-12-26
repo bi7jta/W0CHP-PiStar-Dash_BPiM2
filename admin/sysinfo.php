6bd244d4556f0a51d9dc92248786a5bf70809f38
Chipster <go@w.ay>
2021-12-26 03:36:41 -0600

    Update checker improvements

--- admin/sysinfo.php
+++ admin/sysinfo.php
@@ -126,7 +126,7 @@ function formatSize( $bytes ) {
     <body>
 	<div class="container">
 	    <div class="header">
-		<div style="font-size: 10px; text-align: left; padding-left: 8px; float: left;">Hostname: <?php echo exec('cat /etc/hostname'); ?></div><div style="font-size: 10px; text-align: right; padding-right: 8px;">Pi-Star: <?php echo $_SESSION['PiStarRelease']['Pi-Star']['Version']?> / Dashboard:<?php echo $version; ?></div>
+		<div style="font-size: 10px; text-align: left; padding-left: 8px; float: left;">Hostname: <?php echo exec('cat /etc/hostname'); ?></div><div style="font-size: 10px; text-align: right; padding-right: 8px;">Pi-Star: <?php echo $_SESSION['PiStarRelease']['Pi-Star']['Version']?> / Dashboard:<?php echo $version; echo(exec('/usr/local/sbin/pistar-check4updates')); ?></div>
 		<h1>Pi-Star <?php echo $lang['digital_voice']." ".$lang['dashboard']." - SysInfo";?></h1>
 		<p>
 		    <div class="navbar">
