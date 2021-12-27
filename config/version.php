da25d7634cc9662dcf816a66f605a10a25e8c34f
Chipster <go@w.ay>
2021-12-27 11:07:37 -0600

    versioning enhancements

--- config/version.php
+++ config/version.php
@@ -2,7 +2,7 @@
 
 // Enhanced version status; W0CHP
 
-$ver_cmd = exec('git --work-tree=/var/www/dashboard --git-dir=/var/www/dashboard/.git rev-parse --short HEAD');
+$ver_cmd = exec("git --work-tree=/var/www/dashboard --git-dir=/var/www/dashboard/.git rev-parse HEAD | tail -1 | awk '{ print substr($1,1,10) }')"); # last pipe to awk: converts long hash to 10 chars.
 $ver_no = "W0CHP-PiStar-Dash_Ver.#".$ver_cmd;
 
 $git_branch = exec("git --work-tree=/var/www/dashboard --git-dir=/var/www/dashboard/.git branch | grep '*' | cut -f2 -d ' '");
