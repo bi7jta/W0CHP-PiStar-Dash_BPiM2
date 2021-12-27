3e5a3f184b3281d4f7f71bb8440e57598fc8f8e8
Chipster <go@w.ay>
2021-12-27 13:08:26 -0600

    comment

--- config/version.php
+++ config/version.php
@@ -1,6 +1,7 @@
 <?php
 
 // Enhanced version status; W0CHP
+// Don't fucking touch!
 
 $ver_cmd = exec('git --work-tree=/var/www/dashboard --git-dir=/var/www/dashboard/.git rev-parse HEAD | tail -1 | awk \'{ print substr($1,1,10) }\''); # last pipe to awk: converts long hash to 10 chars.
 $ver_no = "W0CHP-PiStar-Dash_Ver.#".$ver_cmd;
--- index.php
+++ index.php
@@ -67,10 +67,17 @@ checkSessionValidity();
 	</script>
         <link href="/js/select2/css/select2.min.css" rel="stylesheet" />
         <script src="/js/select2/js/select2.min.js"></script>
-         <script>
+         <script type="text/javascript">
           $(document).ready(function() {
             $('.ysfLinkHost').select2();
             $('.RefName').select2();
+          });
+        </script>
+        <script>
+        $(document).ready(function(){
+          setInterval(function(){
+              $("#CheckUpdate").load(window.location.href + " #CheckUpdate" );
+              }, 60000);
           });
         </script>
     </head>
@@ -78,7 +85,7 @@ checkSessionValidity();
 	<div class="container">
 	    <div class="header">
                <div style="font-size: 10px; text-align: left; padding-left: 8px; float: left;">Hostname: <?php echo exec('cat /etc/hostname'); ?></div>
-		<div style="font-size: 10px; text-align: right; padding-right: 8px;">Pi-Star: <?php echo $_SESSION['PiStarRelease']['Pi-Star']['Version'].'<br />';?> <?php echo $lang['dashboard'].": ".$version; echo(exec('/usr/local/sbin/pistar-check4updates')); ?></div>
+		<div style="font-size: 10px; text-align: right; padding-right: 8px;">Pi-Star: <?php echo $_SESSION['PiStarRelease']['Pi-Star']['Version'].'<br />';?><div id="CheckUpdate"><?php echo $lang['dashboard'].": ".$version; echo(exec('/usr/local/sbin/pistar-check4updates')); ?></div></div>
 		<h1>Pi-Star <?php echo $lang['digital_voice']." ".$lang['dashboard_for']." ".$_SESSION['MYCALL']; ?></h1>
 		
 		<p>
