f7214f88129cdf5fd0c1aabb8fe2b508d67cd382
Chipster <go@w.ay>
2021-12-27 07:04:53 -0600

    new version display scheme

--- config/version.php
+++ config/version.php
@@ -2,7 +2,8 @@
 
 // Enhanced version status; W0CHP
 
-$ver_no = '20211227_01-W0CHP';
+$ver_cmd = exec('git --work-tree=/var/www/dashboard --git-dir=/var/www/dashboard/.git rev-parse --short HEAD');
+$ver_no = "W0CHP-PiStar-Dash_Ver.#".$ver_cmd;
 
 $git_branch = exec("git --work-tree=/var/www/dashboard --git-dir=/var/www/dashboard/.git branch | grep '*' | cut -f2 -d ' '");
 $version = "$ver_no / Branch: $git_branch";
