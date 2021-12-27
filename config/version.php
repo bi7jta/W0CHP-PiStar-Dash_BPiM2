0ef4a07963845b25c7a3187eaa7f0685a5d17f40
Chipster <go@w.ay>
2021-12-27 06:39:17 -0600

    updates

--- config/version.php
+++ config/version.php
@@ -1,9 +1,10 @@
 <?php
+
 // Enhanced version status; W0CHP
-$ver_no = '20211226_05-W0CHP';
-//
-//
-//
+
+$ver_no = '20211227_01-W0CHP';
+
 $git_branch = exec("git --work-tree=/var/www/dashboard --git-dir=/var/www/dashboard/.git branch | grep '*' | cut -f2 -d ' '");
 $version = "$ver_no / Branch: $git_branch";
+
 ?>
