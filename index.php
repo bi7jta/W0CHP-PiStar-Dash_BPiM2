5d7c6ea11316de1f405ae8592aba78f18fa6c4ba
Chipster <go@w.ay>
2021-12-27 14:27:50 -0600

    system for echo vs exec

--- index.php
+++ index.php
@@ -85,7 +85,7 @@ checkSessionValidity();
 	<div class="container">
 	    <div class="header">
                <div style="font-size: 10px; text-align: left; padding-left: 8px; float: left;">Hostname: <?php echo exec('cat /etc/hostname'); ?></div>
-		<div style="font-size: 10px; text-align: right; padding-right: 8px;">Pi-Star: <?php echo $_SESSION['PiStarRelease']['Pi-Star']['Version'].'<br />';?><div id="CheckUpdate"><?php echo $lang['dashboard'].": ".$version; echo(exec('/usr/local/sbin/pistar-check4updates')); ?></div></div>
+		<div style="font-size: 10px; text-align: right; padding-right: 8px;">Pi-Star: <?php echo $_SESSION['PiStarRelease']['Pi-Star']['Version'].'<br />';?><div id="CheckUpdate"><?php echo $lang['dashboard'].": ".$version; system('/usr/local/sbin/pistar-check4updates'); ?></div></div>
 		<h1>Pi-Star <?php echo $lang['digital_voice']." ".$lang['dashboard_for']." ".$_SESSION['MYCALL']; ?></h1>
 		
 		<p>
