8bead64da0409a3740801b9573a40d5c59f93ce5
Chipster <go@w.ay>
2022-01-02 06:23:10 -0600

    Experimental DG-ID and M17 support. Not to be fucked with!

--- admin/sysinfo.php
+++ admin/sysinfo.php
@@ -240,7 +240,7 @@ function formatSize( $bytes ) {
 			echo "  <tr>";getStatusClass(isProcessRunning("NXDNGateway"), true); echo "NXDNGateway</td><td align=\"left\">".$NXDNGateway_Ver."</td></tr>\n";
 		    }
 		    if (is_executable('/usr/local/bin/M17Gateway')) {
-			$M17ateway_Ver = exec('/usr/local/bin/M17Gateway -v | cut -d\' \' -f 3-');
+			$M17Gateway_Ver = exec('/usr/local/bin/M17Gateway -v | cut -d\' \' -f 3-');
 			echo "  <tr>";getStatusClass(isProcessRunning("M17Gateway"), true); echo "M17Gateway</td><td align=\"left\">".$M17Gateway_Ver."</td></tr>\n";
 		    }
 		    if (is_executable('/usr/local/bin/NXDN2DMR')) {
