7d95f1538a6ed22b594ae8ad4c539f464ada064f
Chipster <go@w.ay>
2022-01-02 03:26:44 -0600

    Minor: Improved version checking function.

--- config/version.php
+++ config/version.php
@@ -2,10 +2,10 @@
 
 // Enhanced version status; W0CHP
 
-$ver_cmd = exec('git --work-tree=/var/www/dashboard --git-dir=/var/www/dashboard/.git rev-parse HEAD | tail -1 | awk \'{ print substr($1,1,10) }\''); # last pipe to awk: converts long hash to 10 chars.
-$ver_no = "W0CHP-PiStar-Dash_Ver.#".$ver_cmd;
+$gitBranch = exec("git --work-tree=/var/www/dashboard --git-dir=/var/www/dashboard/.git branch | grep '*' | cut -f2 -d ' '");
+$versionCmd = exec("git --work-tree=/var/www/dashboard --git-dir=/var/www/dashboard/.git rev-parse --short=10 $gitBranch");
+$verNo = "W0CHP-PiStar-Dash_Ver.#".$versionCmd;
 
-$git_branch = exec("git --work-tree=/var/www/dashboard --git-dir=/var/www/dashboard/.git branch | grep '*' | cut -f2 -d ' '");
-$version = "$ver_no / Branch: $git_branch";
+$version = "$verNo / Branch: $gitBranch";
 
 ?>
