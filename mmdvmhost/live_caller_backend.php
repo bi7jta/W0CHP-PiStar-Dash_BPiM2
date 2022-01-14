ef3e27d4da78da7338b4034b0b78725f760becda
Chipster <go@w.ay>
2022-01-14 11:48:48 -0600

    * Minor: Improved Live Caller screen some more. Using native php fopen() with buffer, into array; rather than 4 sep. exec("grep ...") funcs.'

--- mmdvmhost/live_caller_backend.php
+++ mmdvmhost/live_caller_backend.php
@@ -93,10 +93,24 @@ elseif (floatval($listElem[8]) >= 0.0 && floatval($listElem[8]) <= 1.1)
 	$ber = "<span class='ber_bad'>".$listElem[8]."</span>";
 }
 
-$name = exec("grep -w \"$listElem[2]\" /usr/local/etc/stripped.csv | awk -F, '{print $3, $4}' | head -1 | tr -d '\"' ");
-$city = exec("grep -w \"$listElem[2]\" /usr/local/etc/stripped.csv | awk -F, '{print $5}' | head -1 | tr -d '\"' ");
-$state = exec("grep -w \"$listElem[2]\" /usr/local/etc/stripped.csv | awk -F, '{print $6}' | head -1 | tr -d '\"' ");
-$country = exec("grep -w \"$listElem[2]\" /usr/local/etc/stripped.csv | awk -F, '{print $7}' | head -1 | tr -d '\"' ");
+$searchCall = $listElem[2];
+$callMatch = array();
+$handle = @fopen("/usr/local/etc/stripped.csv", "r");
+if ($handle)
+{
+    while (!feof($handle))
+    {
+        $buffer = fgets($handle);
+        if(strpos($buffer, $searchCall) !== FALSE)
+            $callMatch[] = $buffer;
+    }
+    fclose($handle);
+}
+$callMatch= explode(",", $callMatch[0]);
+$name = "$callMatch[2] $callMatch[3]";
+$city = $callMatch[4];
+$state = $callMatch[5];
+$country = $callMatch[6]; 
 
 if (strlen($target) >= 2) {
 	$target_lookup = exec("grep -w \"$target\" /usr/local/etc/groups.txt | awk -F, '{print $1}' | head -1 | tr -d '\"'");
