6e4ed471b22ccdb57cb33a355a9efc82c555c809
Chipster <go@w.ay>
2022-02-04 05:04:28 -0600

    * Minor: UI update...supress branch name in header if on master

--- config/version.php
+++ config/version.php
@@ -6,6 +6,10 @@ $gitBranch = exec("git --work-tree=/var/www/dashboard --git-dir=/var/www/dashboa
 $versionCmd = exec("git --work-tree=/var/www/dashboard --git-dir=/var/www/dashboard/.git rev-parse --short=10 $gitBranch");
 $verNo = "W0CHP-PiStar-Dash_Ver.#".$versionCmd;
 
-$version = "$verNo / Branch: $gitBranch";
+if ($gitBranch !== "master") {
+    $version = "$verNo / Branch: $gitBranch";
+} else {
+    $version = "$verNo";
+}
 
 ?>
