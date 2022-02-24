3642cdbfbe95a1bb630039b306a8da362120645b
Chipster <go@w.ay>
2022-02-23 20:13:21 -0600

    * Minor: disable upgr. message, as all installations cannot happen w.out 4.1.6 anyway.

--- config/messages.php
+++ config/messages.php
@@ -1,18 +1,15 @@
 <?php
+/*
 require_once($_SERVER['DOCUMENT_ROOT'].'/mmdvmhost/functions.php');
-
 $min_ver = "4.1.6";
-
 $headers = stream_context_create(Array("http" => Array("method"  => "GET",
                                                        "timeout" => 1,
                                                        "header"  => "User-agent: WPSD-Messages",
-                                                       'request_fulluri' => True /* without this option we get an HTTP error! */
-				)));
-
+                                                       'request_fulluri' => True )));
 $string = $_SESSION['PiStarRelease']['Pi-Star']['Version'];
 if ($string < $min_ver) {
 	$result = @file_get_contents('https://w0chp.net/WPSD/upgrade_required.html', false, $headers);
 	echo $result;
 }
-
+*/
 ?>
