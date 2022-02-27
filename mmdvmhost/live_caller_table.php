34dd5fa9863d8944de9dce9237b0fdbf5143efe1
Chipster <go@w.ay>
2022-02-27 11:10:20 -0600

    * Minor: fix Current/Last Caller table logic to supress TX info when there isn't any (e.g. bootup/service start).

--- mmdvmhost/live_caller_table.php
+++ mmdvmhost/live_caller_table.php
@@ -9,7 +9,20 @@ if (constant("TIME_FORMAT") == "24") {
 } else {
     $local_time = date('h:i:s A M. jS');
 }
+?>
 
+<div style="vertical-align: bottom; font-weight: bold;text-align:left;">Current / Last Caller Details</div>
+  <table style="word-wrap: break-word; white-space:normal;">
+    <tr>
+      <th>Callsign</th>
+      <th>Name</th>
+      <th>Location</th>
+      <th>Source</th>
+      <th>Mode</th>
+      <th>Target</th>
+      <th>Duration</th>
+    </tr>
+<?php
 // get the data from the MMDVMHost logs
 $i = 0;
 for ($i = 0;  ($i <= 0); $i++) { //Last 20  calls
@@ -40,133 +53,117 @@ for ($i = 0;  ($i <= 0); $i++) { //Last 20  calls
 		    $callsign = "<a href=\"http://www.qrz.com/db/$listElem[2]\" target=\"_blank\">$listElem[2]</a>";
 		}
             }
-        }
-    }
-}
 
-if ( substr($listElem[4], 0, 6) === 'CQCQCQ' ) {
-	$target = $listElem[4];
-} else {
-	$target = str_replace(" ","&nbsp;", $listElem[4]);
-}
+	    if ( substr($listElem[4], 0, 6) === 'CQCQCQ' ) {
+		$target = $listElem[4];
+	    } else {
+		$target = str_replace(" ","&nbsp;", $listElem[4]);
+	    }
 		
-$target = preg_replace('/TG /', '', $listElem[4]);
+	    $target = preg_replace('/TG /', '', $listElem[4]);
 
-$source = $listElem[5];
+	    $source = $listElem[5];
 	
-if ($listElem[6] == null) {
-	// Live duration
-	$utc_time = $listElem[0];
-	$utc_tz =  new DateTimeZone('UTC');
-	$now = new DateTime("now", $utc_tz);
-	$dt = new DateTime($utc_time, $utc_tz);
-	$duration = $now->getTimestamp() - $dt->getTimestamp();
-	$duration_string = $duration<999 ? round($duration) . "+" : "&infin;";
-	$duration = "<td style=\"background:#d11141;color:#fff;\">TX " . $duration_string . " sec</td>";
-} else if ($listElem[6] == "DMR Data") {
-	$duration =  "<td style=\"background:#00718F;color:#fff;\">DMR Data</td>";
-} else if ($listElem[6] == "POCSAG Data") {
-	$duration =  "<td style=\"background:#00718F;color:#fff;\">POCSAG Data</td>";
-} else {
-	$duration = "<td>$listElem[6]s</td>";
-}
+	    if ($listElem[6] == null) {
+		// Live duration
+		$utc_time = $listElem[0];
+		$utc_tz =  new DateTimeZone('UTC');
+		$now = new DateTime("now", $utc_tz);
+		$dt = new DateTime($utc_time, $utc_tz);
+		$duration = $now->getTimestamp() - $dt->getTimestamp();
+		$duration_string = $duration<999 ? round($duration) . "+" : "&infin;";
+		$duration = "<td style=\"background:#d11141;color:#fff;\">TX " . $duration_string . " sec</td>";
+	    } else if ($listElem[6] == "DMR Data") {
+		$duration =  "<td style=\"background:#00718F;color:#fff;\">DMR Data</td>";
+	    } else if ($listElem[6] == "POCSAG Data") {
+		$duration =  "<td style=\"background:#00718F;color:#fff;\">POCSAG Data</td>";
+	    } else {
+		$duration = "<td>$listElem[6]s</td>";
+	    }
 
-if ($listElem[8] == null) {
-	$ber = "&nbsp;";
-} else {
-	$mode = $listElem[8];
-}
+	    if ($listElem[8] == null) {
+		$ber = "&nbsp;";
+	    } else {
+		$mode = $listElem[8];
+	    }
 
-if ($listElem[1] == null) {
-	$ber = "&nbsp;";
-} else {
-	$mode = $listElem[1];
-}
+	    if ($listElem[1] == null) {
+		$ber = "&nbsp;";
+	    } else {
+		$mode = $listElem[1];
+	    }
 			
-if (!is_numeric($listElem[2])) {
-        $searchCall = $listElem[2];
-        $callMatch = array();
-	if ($mode == "NXDN") {
-		$handle = @fopen("/usr/local/etc/NXDN.csv", "r");
-	} else { # all other modes
-		$handle = @fopen("/usr/local/etc/stripped.csv", "r");
-	}
-        if ($handle)
-        {       
-                while (!feof($handle))
-                {
-                        $buffer = fgets($handle);
-                        if (strpos($buffer, $searchCall) !== FALSE)
-                        {
-                                $csvBuffer = explode(",", $buffer);
-                                if(strpos($searchCall, $csvBuffer[1]) !== FALSE)
-                                        $callMatch[] = $buffer;
-                        }
-                }
-                fclose($handle);
-        }
-        $callMatch = explode(",", $callMatch[0]);
-        $name = "$callMatch[2] $callMatch[3]";
-        $city = $callMatch[4];
-        $state = $callMatch[5];
-        $country = $callMatch[6];
-}
+	    if (!is_numeric($listElem[2])) {
+        	$searchCall = $listElem[2];
+        	$callMatch = array();
+		if ($mode == "NXDN") {
+		    $handle = @fopen("/usr/local/etc/NXDN.csv", "r");
+		} else { # all other modes
+		    $handle = @fopen("/usr/local/etc/stripped.csv", "r");
+		}
+        	if ($handle)
+        	{ 
+                    while (!feof($handle))
+                    {
+                	$buffer = fgets($handle);
+                	if (strpos($buffer, $searchCall) !== FALSE)
+                	{
+			    $csvBuffer = explode(",", $buffer);
+			    if(strpos($searchCall, $csvBuffer[1]) !== FALSE)
+				$callMatch[] = $buffer;
+			}
+		    }
+		    fclose($handle);
+		}
+		$callMatch = explode(",", $callMatch[0]);
+		$name = "$callMatch[2] $callMatch[3]";
+		$city = $callMatch[4];
+		$state = $callMatch[5];
+		$country = $callMatch[6];
+	    }
 
-if (strlen($target) >= 2) {
-    if (strpos($mode, 'DMR') !== false) {
-        $target_lookup = exec("grep -w \"$target\" /usr/local/etc/groups.txt | awk -F, '{print $1}' | head -1 | tr -d '\"'");
-        if (!empty($target_lookup)) {
-                $target = $target_lookup;
-                $stupid_bm = ['/ - 10 Minute Limit/', '/ NOT A CALL CHANNEL/', '/ NO NETS(.*?)/', '/ - .*/'];
-                $target = preg_replace($stupid_bm, "", $target); // strip stupid fucking comments from BM admins in TG names. Idiots.
-                $target = str_replace(":", " - ", $target);
-                $target = "TG $target";
-        } else {
-                $target = "TG $target";
-        }
-    } else if (strpos($mode, 'NXDN') !== false) {
-        $target_lookup = exec("grep -w \"$target\" /usr/local/etc/TGList_NXDN.txt | awk -F';' '{print $2}'");
-        if (!empty($target_lookup)) {
-                $target = "TG $target - $target_lookup";
-        }
-    } else if (strpos($mode, 'P25') !== false) {
-        $target_lookup = exec("grep -w \"$target\" /usr/local/etc/TGList_P25.txt | awk -F';' '{print $2}'");
-        if (!empty($target_lookup)) {
-                $target = "TG $target - $target_lookup";
-        }
-    } else {
-        $target = $target;
-    }
-} else {
-    $modeArray = array('DMR', 'NXDN', 'P25');
-    if (strpos($mode, $modeArray[0]) !== false) {
-        $target = "TG $target";
-    } else {
-        $target = $target;
-    }
-}
+	    if (strlen($target) >= 2) {
+		if (strpos($mode, 'DMR') !== false) {
+		    $target_lookup = exec("grep -w \"$target\" /usr/local/etc/groups.txt | awk -F, '{print $1}' | head -1 | tr -d '\"'");
+		    if (!empty($target_lookup)) {
+			$target = $target_lookup;
+			$stupid_bm = ['/ - 10 Minute Limit/', '/ NOT A CALL CHANNEL/', '/ NO NETS(.*?)/', '/ - .*/'];
+			$target = preg_replace($stupid_bm, "", $target); // strip stupid fucking comments from BM admins in TG names. Idiots.
+			$target = str_replace(":", " - ", $target);
+			$target = "TG $target";
+		    } else {
+			$target = "TG $target";
+		    }
+		} else if (strpos($mode, 'NXDN') !== false) {
+		    $target_lookup = exec("grep -w \"$target\" /usr/local/etc/TGList_NXDN.txt | awk -F';' '{print $2}'");
+		    if (!empty($target_lookup)) {
+			$target = "TG $target - $target_lookup";
+		    }
+		} else if (strpos($mode, 'P25') !== false) {
+		    $target_lookup = exec("grep -w \"$target\" /usr/local/etc/TGList_P25.txt | awk -F';' '{print $2}'");
+			if (!empty($target_lookup)) {
+			    $target = "TG $target - $target_lookup";
+			}
+		} else {
+		    $target = $target;
+		}
+	    } else {
+		$modeArray = array('DMR', 'NXDN', 'P25');
+		if (strpos($mode, $modeArray[0]) !== false) {
+		    $target = "TG $target";
+		} else {
+		    $target = $target;
+		}
+	    }
 
-if($listElem[2] == "4000" || $listElem[2] == "9990" || $listElem[2] == "DAPNET") {
-	$name = "";
-	$city = "";
-	$state = "";
-	$country = "";
-	$duration = "<td></td>";
-}
-
+	    if($listElem[2] == "4000" || $listElem[2] == "9990" || $listElem[2] == "DAPNET") {
+		$name = "";
+		$city = "";
+		$state = "";
+		$country = "";
+		$duration = "<td></td>";
+	    }
 ?>
-
-<div style="vertical-align: bottom; font-weight: bold;text-align:left;">Current / Last Caller Details</div>
-  <table style="word-wrap: break-word; white-space:normal;">
-    <tr>
-      <th>Callsign</th>
-      <th>Name</th>
-      <th>Location</th>
-      <th>Source</th>
-      <th>Mode</th>
-      <th>Target</th>
-      <th>Duration</th>
-    </tr>
   <tr>
     <td><?php echo $callsign ?? ' '; ?></td>
     <td><?php echo $name ?? ' '; ?></td>
@@ -188,8 +185,14 @@ if($listElem[2] == "4000" || $listElem[2] == "9990" || $listElem[2] == "DAPNET")
     ?>
     <td><?php echo $mode ?? ' '; ?></td>
     <td><?php echo $target ?? ' '; ?></td>
-    <?php echo $duration ?? ' '; ?>
+    <?php echo $duration; ?>
    </tr>
+<?php
+	    }
+	}
+    }
+}
+?>
 </table>
 <br />
-<?php } ?>
+
