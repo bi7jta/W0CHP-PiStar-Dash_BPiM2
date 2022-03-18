2d8747f46aacda6ceb96d0e9716061fc6e9be9a1
Chipster <go@w.ay>
2022-03-18 13:52:58 -0500

    * Minor: let cron re-enable read-only when applying configs to prevent R/O errors.

--- admin/configure.php
+++ admin/configure.php
@@ -3826,7 +3826,7 @@ if (!empty($_POST)):
     
 	// Start all services
 	system('sudo systemctl daemon-reload > /dev/null 2>/dev/null &');	// Restart Systemd to account for any service changes
-	system('sudo pistar-services start > /dev/null 2>/dev/null &');
+	system('sudo REMOUNT_RO="NO" pistar-services start > /dev/null 2>/dev/null &');
 
 	unset($_POST);
 	echo '<script type="text/javascript">setTimeout(function() { window.location=window.location;},7500);</script>';
