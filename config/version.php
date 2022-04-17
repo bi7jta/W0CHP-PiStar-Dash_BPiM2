c1e7638317368f4392be63a2d09761ba7b9678ed
Chipster <go@w.ay>
2022-04-17 15:24:48 -0500

    * Minor: new binaries released; version bump to update configs

--- config/version.php
+++ config/version.php
@@ -3,7 +3,7 @@
 // Enhanced version status; W0CHP
 
 // format = YYYYMMDDnn
-$configUpdateRequired = 2022040800;
+$configUpdateRequired = 2022041700;
 
 $gitBranch = exec("git --work-tree=/var/www/dashboard --git-dir=/var/www/dashboard/.git branch | grep '*' | cut -f2 -d ' '");
 $versionCmd = exec("git --work-tree=/var/www/dashboard --git-dir=/var/www/dashboard/.git rev-parse --short=10 $gitBranch");
