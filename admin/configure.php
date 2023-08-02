b7d7b056e8dfc9e5c7d0911d5364a0171e7ddcb5
BI7JTA <bi7jta@gmail.com>
2023-08-02 15:10:50 +0800

    Remove Public mode and bmkey warning...

--- admin/configure.php
+++ admin/configure.php
@@ -505,35 +505,9 @@ $MYCALL=strtoupper($callsign);
     <script type="text/javascript" src="/js/functions.js?version=<?php echo $versionCmd; ?>"></script>
 </head>
 <body onload="checkFrequency(); return false;">
-<?php
-if ( (file_exists('/etc/dstar-radio.mmdvmhost') && $configmmdvm['DMR']['Enable'] == 1 && $configmmdvm['DMR']['SelfOnly'] == 0 && strlen($configmmdvm['General']['Id']) >= 7 ) && (!isset($configmmdvm['DMR']['WhiteList'])) ) {
-?>
-<div>
-  <table align="center" width="760px" style="margin: 0px 0px 10px 0px; width: 100%;">
-    <tr>
-    <td align="center" valign="top" style="background-color: #ffff90; color: #906000;">Alert: You are running a hotspot in public mode without an access list for DMR, this setup *could* participate in network loops!</td>
-    </tr>
-  </table>
-</div>
-<?php
-}
-?>
-<?php
-$bmAPIkeyFile = '/etc/bmapi.key';
-if (file_exists($bmAPIkeyFile) && fopen($bmAPIkeyFile,'r')) {
-  $configBMapi = parse_ini_file($bmAPIkeyFile, true);
-  $bmAPIkey = $configBMapi['key']['apikey'];
-  // Check the BM API Key
-  if ( strlen($bmAPIkey) <= 200 ) {
-?>
-<div>
-  <table align="center"style="margin: 0px 0px 10px 0px; width: 100%;border-collapse:collapse; table-layout:fixed;white-space: normal!important;">
-    <tr>
-    <td align="center" valign="top" style="background-color: #ffff90; color: #906000; word-wrap: break-all;padding:20px;">Notice! You have a legacy Brandmeister API v1 Key. Read the announcement on how to migrate: <a href="https://news.brandmeister.network/introducing-user-api-keys/" target="new" alt="BM API Keys">BM API Key Announcement and Migration Instructions</a>; and then <a href="/admin/expert/fulledit_bmapikey.php">Update your API Key</a>.</td>
-    </tr>
-  </table>
-</div>
-<?php } } ?>
+
+<!-- Remove Public mode and bmkey warning...-->
+
 <div class="container">
 <div class="header">
 <div style="font-size: 10px; text-align: left; padding-left: 8px; float: left;">Hostname: <?php echo exec('cat /etc/hostname'); ?></div>
